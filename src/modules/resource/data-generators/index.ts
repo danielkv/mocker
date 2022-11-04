ad5333d10025cbb0d7cad4e33854a3365c2381a0
Daniel K. Guolo <daniel_kv@hotmail.com>
2022-11-04 10:52:36 -0300

    feat: generator

--- src/modules/resource/data-generators/index.ts
+++ src/modules/resource/data-generators/index.ts
@@ -1,12 +1,15 @@
 import { ValueProvider } from '@nestjs/common';
 
 import { DataGeneratorProvider } from '../interfaces/data-generator';
+import { NumberGenerator } from './number.generator';
+import { OneOfGenerator } from './oneof.generator';
 import { StringGenerator } from './string.generator';
 
 const generators: DataGeneratorProvider = {
     string: new StringGenerator(),
-    list: new StringGenerator(),
-    number: new StringGenerator(),
+    oneof: new OneOfGenerator(),
+    number: new NumberGenerator(),
+    boolean: new NumberGenerator(),
 };
 
 export const GENERATORS = 'GENERATORS_PROVIDER';
